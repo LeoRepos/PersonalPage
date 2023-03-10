36d1cb8e246b7a0525b684a5c5953c7ffb87fe35
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2023-03-10 16:30:58 -0300

    feat/ adicionando texto animado

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+//* variaveis para o slider
 let totalSliders = document.querySelectorAll('.slider--item').length;
 let currentSlide = 0;
+
+//* variaveis para o texto animado
+var i            = 0;
+var tag          = document.getElementById("text");
+var html         = document.getElementById("text").innerHTML;
+var attr         = tag.setAttribute("data", html);
+var txt          = tag.getAttribute("data");
+var speed        = 170;
 
 document.querySelector('.slider--width').style.width     = `calc(100vw * ${totalSliders})`;
 document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
 
+//* area do slide 
 function goPrev() {
     currentSlide--;
     if ( currentSlide < 0) {
@@ -25,5 +35,14 @@ function updateMargin() {
     let newMargin       = (currentSlide * sliderItemWidth);
     document.querySelector('.slider--width').style.marginLeft =  `-${newMargin}px`;
 }
-
 setInterval(goNext,3000);
+
+//* método do texto animado
+    function typeWriter() {
+      if (i <= txt.length) {
+        document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+typeWriter();
